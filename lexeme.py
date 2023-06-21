6775bd084d29b1c8cd2888459127660a420d1dd9
Pauline Sander <sanderpe@steppenweihe.ims.uni-stuttgart.de>
2023-06-21 16:11:33 +0200

    remove model_output_value from call in lexeme.py

--- lexeme.py
+++ lexeme.py
@@ -31,8 +31,8 @@ def make_inference_for_dataset(path_of_dataset,subword_aggregation,prediction_ty
     # comment/uncomment the following 3  lines as the WordTransformer module is not executing successfully because of installation issue with the sqlite3 on server for the moment i am computing embeddings on my local machine and copying the embeddings in the temp folder and loading them here
 
     model = WordTransformer('pierluigic/xl-lexeme')
-    save_embeddings_lexeme(device, left_sentnece_and_token_index, './temp/token_embeddings_left.npy', model,subword_aggregation,model_output_value)
-    save_embeddings_lexeme(device, right_sentnece_and_token_index, './temp/token_embeddings_right.npy', model,subword_aggregation,model_output_value)
+    save_embeddings_lexeme(device, left_sentnece_and_token_index, './temp/token_embeddings_left.npy', model,subword_aggregation)
+    save_embeddings_lexeme(device, right_sentnece_and_token_index, './temp/token_embeddings_right.npy', model,subword_aggregation)
 
     embeddings_left = np.load('./temp/token_embeddings_left.npy')
 
