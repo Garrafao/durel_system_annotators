0f12ec50ec0bc915d5f366b772b2e29175ddb5bc
Pauline Sander <sanderpe@steppenweihe.ims.uni-stuttgart.de>
2023-09-21 13:30:59 +0200

    New authentication

--- authenticator.py
+++ authenticator.py
@@ -1,15 +1,20 @@
 import requests
 import configparser
+from bs4 import BeautifulSoup
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 url = config['SERVER']['url'] + config['SERVER']['api_version'] + config['AUTH-ROUTES']['login']
 
-r = requests.post(url, json={
-    'username': config['CREDENTIALS']['username'],
-    'password': config['CREDENTIALS']['password']
-})
+with requests.Session() as s:
+    res = s.get("https://durel.ims.uni-stuttgart.de/login")
+    login = BeautifulSoup(res._content, 'html.parser')
+    p = s.post("https://durel.ims.uni-stuttgart.de/login", data={'username': 'PaulineSander', 'password': 'Ju7pZgmSwCzFjUv'})
+    r = s.post(url, json={
+        'username': config['CREDENTIALS']['username'],
+        'password': config['CREDENTIALS']['password']})
+    print(r.content)
 
 if r.status_code == 200:
     # Save token to config file 
