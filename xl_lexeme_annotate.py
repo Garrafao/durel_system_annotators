dad50285b38241f0923b2ff847c4d80819f56ae3
AinaIanemahy <paulinesander@gmx.de>
2024-02-21 13:33:23 +0100

    Refactor xl_lexeme_annotate.py and update settings
    xl_lexeme_annotate.py has been refactored to improve readability and maintainability. This involved restructuring the main function, splitting functionality into smaller, dedicated functions. In addition, the settings repository was updated to include new judgment_columns and token_index_filename, enhancing the configuration options for the annotations.

--- xl_lexeme_annotate.py
+++ xl_lexeme_annotate.py
@@ -1,17 +1,48 @@
+import json
 from optparse import OptionParser
-import logging
+import random
 
 from annotation_provider import AnnotationProvider
 from lexeme import *
 
-def main(usage_dir, custom_dir, custom_filename, prefix, debug,subword_aggregation,prediction_type,thresholds):
+
+def main(annotator, usage_dir, custom_dir, custom_filename, prefix, debug, thresholds):
+    check_annotation_input_and_logging(annotator, debug, usage_dir, custom_dir, custom_filename)
+
+    annotation_provider = AnnotationProvider(usage_dir, prefix, DEBUG=debug)
+
+    cls_result = None
+    if annotator == "XL-Lexeme":
+        annotation_provider.flush_instance_with_token_index(path=custom_dir)
+        # TODO fill in columns and delimiter
+        columns = None
+        delimiter = None
+        cls_result = make_inference_for_dataset(custom_dir + '/{}'.format(prefix) + settings['token_index_filename'],
+                                                delimiter, columns, thresholds)
+        annotator = specify_xl_lexeme_annotator(thresholds)
+
+    for i, instance in enumerate(annotation_provider.get_instances_iterator(RANDOM=False)):
+        judgment = cls_result[i] if cls_result is not None else random.choice([*instance['label_set']])
+        columns = settings['judgment_columns']
+        # TODO the column structure shouldn't be hardcoded here.
+        annotation_provider.add_judgement({columns[0]: instance['lemma'], columns[1]: instance['instanceID'],
+                                           columns[2]: instance['internal_identifier1'], columns[3]:
+                                               instance['internal_identifier2'],
+                                           columns[4]: judgment, columns[5]: '-'})
+
+    # Save the judgement
+    annotation_provider.flush_judgement(path=custom_dir, filename=custom_filename, annotator=annotator)
+
+
+def check_annotation_input_and_logging(annotator, debug, usage_dir, custom_dir, custom_filename):
+
+    if debug:
+        logging.info(f"Using annotator '{annotator}' to store judgements.")
 
     if debug:
         logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
         logging.info("Debug mode is on.")
 
-    if usage_dir is None:
-        raise ValueError("usage_dir is None")
     if debug:
         logging.info(f"Using directory '{usage_dir}' for usage data.")
 
@@ -20,42 +51,41 @@ def main(usage_dir, custom_dir, custom_filename, prefix, debug,subword_aggregati
     if debug:
         logging.info(f"Using directory '{custom_dir}' to store judgements.")
 
-    if custom_filename is None:
-        custom_filename = "judgements.csv"
     if debug:
         logging.info(f"Using filename '{custom_filename}' to store judgements.")
 
-    if prediction_type == "label":
-        annotator = "XL-Lexeme-Binary"
+
+def specify_xl_lexeme_annotator(thresholds: list[int]) -> str:
+    if thresholds is not None and len(thresholds) == 1:
+        return "XL-Lexeme-Binary"
+    elif thresholds is not None and len(thresholds) == 3:
+        return "XL-Lexeme-Multi-Threshold"
     else:
-        annotator = "XL-Lexeme-Cosine"
+        return "XL-Lexeme-Cosine"
 
-    # Example annotator that randomly annotates the given set
-    annotation_provider = AnnotationProvider(usage_dir, prefix, DEBUG=debug)
-
-    annotation_provider.flush_instance_with_token_index(path=custom_dir)
-
-    cls_result = make_inference_for_dataset(custom_dir+'/{}instances_with_token_index.csv'.format(prefix),subword_aggregation,prediction_type,thresholds)
-
-    for i, instance in enumerate(annotation_provider.get_instances_iterator(RANDOM=False)):
-        #print({'instanceID': instance['instanceID'], 'internal_identifier1': instance['internal_identifier1'], 'internal_identifier2': instance['internal_identifier2'], 'label': cls_result[i], 'comment': '-'})
-        annotation_provider.add_judgement({'lemma': instance['lemma'], 'instanceID': instance['instanceID'], 'identifier1': instance['internal_identifier1'], 'identifier2': instance['internal_identifier2'], 'judgment': cls_result[i], 'comment': '-'})
-
-    # Save the judgement
-    annotation_provider.flush_judgement(path=custom_dir, filename=custom_filename, annotator=annotator)
 
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option("-u", "--usage_dir", dest="usage_dir", help="Directory containing uses and instances data")
-    parser.add_option("-p", "--prefix", dest="prefix", help="Prefix for the usage, instance and judgement files")
-    parser.add_option("-c", "--custom_dir", dest="custom_dir", help="Directory to store custom judgements")
-    parser.add_option("-f", "--custom_filename", dest="custom_filename", help="Filename to store custom judgements")
-    parser.add_option("-d", "--debug", dest="debug", action="store_true", help="Enable debug mode")
-    parser.add_option("-s", "--subword", dest="subword", help="provide subword subword_aggregation")
-    parser.add_option("-o", "--output", dest="output", help="output type either lable or distance")
-    (options, args) = parser.parse_args()
-    print(options)
-    thresholds = [0.2,0.4,0.6]
-    main(options.usage_dir, options.custom_dir, options.custom_filename, options.prefix, options.debug, options.subword, options.output,thresholds)
+    parser.add_option("-a", "--annotator", dest="annotator", default="XL-Lexeme",
+                      help="Should be XL-Lexeme or Random. Default XL-Lexeme.")
 
-# python xlmr_naive_annotate.py -u tmp -p "" -d
+    parser.add_option("-p", "--prefix", dest="prefix", help="Prefix for the usage, instance and judgement files.")
+    parser.add_option("-u", "--usage_dir", dest="usage_dir", required=True,
+                      help="Directory containing uses and instances data.")
+    parser.add_option("-c", "--custom_dir", dest="custom_dir", help="Directory to store custom judgements.")
+    parser.add_option("-f", "--custom_filename", dest="custom_filename", default="judgements.csv",
+                      help="Filename to store custom judgements.")
+
+    parser.add_option("-d", "--debug", dest="debug", action="store_true", help="Enable debug mode.")
+    parser.add_option("-t", "--thresholds", dest="thresholds", type=list[int], default=None,
+                      help="Thresholds for cutoff if mapping to labels is requested.")
+    parser.add_option("-s", "--sub-word", dest="sub-word", help="provide sub-word subword_aggregation.")
+    (options, args) = parser.parse_args()
+    logging.info(options)
+    default_thresholds = [0.2, 0.4, 0.6]
+
+    with open('settings.json') as settings_file:
+        settings = json.load(settings_file)
+
+    main(options.annotator, options.usage_dir, options.custom_dir, options.custom_filename, options.prefix,
+         options.debug, options.thresholds)
