4c1eed39ff2c021373f14d10574ad666d66c0b7e
AinaIanemahy <paulinesander@gmx.de>
2024-02-26 09:24:32 +0100

    Refactor annotation process and improve code readability
    This commit refactors the main annotation process and enhances the code readability by adding detailed comments. Logging functionalities were improved, and now settings including columns and delimiter are loaded from the settings file. Instead of judgments, the term "annotations" is used consistently throughout the code. The script was also designed to be more adaptable to adding new annotators in the future.

--- code/annotate.py
+++ code/annotate.py
@@ -4,67 +4,75 @@ from optparse import OptionParser
 
 from annotation_provider import AnnotationProvider
 from xl_lexeme import *
+# For new annotators:
+# from NEW_ANNOTATOR_NAME import *
+
+"""
+This script handles the main annotation process, i.e. reading the input files, calling the specified annotator and 
+writing the annotations to the output annotation file.
+"""
 
 
-def main(annotator, usage_dir, custom_dir, custom_filename, prefix, debug, thresholds):
-    check_annotation_input_and_logging(annotator, debug, usage_dir, custom_dir, custom_filename)
+def main(annotator, usage_dir, annotation_dir, annotation_filename, prefix, debug, thresholds, settings_location):
+    """
 
-    with open('./settings/repository-settings.json') as settings_file:  # to do: should this be an input argument?
+    :param annotator: The name of the annotator to use. It should be one of the available annotators.
+    :param usage_dir: The directory where the usage data is stored.
+    :param annotation_dir: The directory where annotation data is stored.
+    :param annotation_filename: The filename of the custom data.
+    :param prefix: The prefix to use for custom data files.
+    :param debug: A boolean flag indicating whether debug mode is enabled.
+    :param thresholds: A dictionary containing the thresholds for the annotator.
+    :param settings_location: The location of the settings file.
+
+    This method performs the main functionality of the program. It checks the annotation input and logging, loads the
+    settings file, creates an annotation provider, and generates annotations based on the chosen annotator.
+    The annotations are then added to the annotation provider and saved to a file.
+
+    Note: To add another annotator, you need to add the appropriate code within the if-elif block.
+
+    """
+    # Setup
+    with open(settings_location) as settings_file:
         settings = json.load(settings_file)
 
-    annotation_provider = AnnotationProvider(usage_dir, prefix, DEBUG=debug)
+    annotation_input_logging(annotator, debug, usage_dir, annotation_dir, annotation_filename)
+    annotation_provider = AnnotationProvider(usage_dir, prefix, debug=debug)
 
-    cls_result = None
+    # Get judgments for annotators other than random
+    judgments = None
     if annotator == "XL-Lexeme":
-        annotation_provider.flush_instance_with_token_index(path=custom_dir)
-        # TODO fill in columns and delimiter
-        columns = ["word", "sentence_left", "token_index_of_sentence_left",
-                   "sentence_right", "token_index_of_sentence_right"]
-        delimiter = "\t"
-        cls_result = make_inference_for_dataset(custom_dir + '/{}'.format(prefix) + settings['token_index_filename'],
-                                                delimiter, columns, thresholds)
+        annotation_provider.flush_instance_with_token_index(path=annotation_dir)
+        columns = settings['annotations_columns']
+        delimiter = settings['delimiter']
+        token_index_filepath = annotation_dir + '/{}'.format(prefix) + settings['token_index_filename']
+        judgments = make_inference_for_dataset(token_index_filepath, delimiter, columns, thresholds)
         annotator = specify_xl_lexeme_annotator(thresholds)
 
-    for i, instance in enumerate(annotation_provider.get_instances_iterator(RANDOM=False)):
-        judgment = cls_result[i] if cls_result is not None else random.choice([*instance['label_set']])
-        columns = settings['judgment_columns']
-        # TODO the column structure shouldn't be hardcoded here.
-        annotation_provider.add_judgement({columns[0]: instance['lemma'], columns[1]: instance['instanceID'],
-                                           columns[2]: instance['internal_identifier1'], columns[3]:
-                                               instance['internal_identifier2'],
-                                           columns[4]: judgment, columns[5]: '-'})
+    # If you want to add another annotator, add the following here:
+    # elif annotator == "NEW_ANNOTATOR_NAME":
+    #       code here
+    #       cls_result = some_function_in_NEW_ANNOTATOR_NAME.py()
 
-    # Save the judgement
-    annotation_provider.flush_judgement(path=custom_dir, filename=custom_filename, annotator=annotator)
+    # Create annotations in the annotation provider
+    for i, instance in enumerate(annotation_provider.get_instances_iterator(random_order=False)):
+        judgment = judgments[i] if judgments is not None else random.choice([*instance['label_set']])
+        columns = settings['annotations_columns']
+        annotation = annotation_provider.instance_to_annotation(instance, judgment, columns)
+        annotation_provider.add_annotation(annotation)
+
+    # Save the annotations
+    annotation_provider.flush_annotation(path=annotation_dir, filename=annotation_filename, annotator=annotator)
 
 
-def check_annotation_input_and_logging(annotator, debug, usage_dir, custom_dir, custom_filename):
-    if debug:
-        logging.info(f"Using annotator '{annotator}' to store judgements.")
-
+def annotation_input_logging(annotator, debug, usage_dir, annotation_dir, annotation_filename):
     if debug:
         logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
         logging.info("Debug mode is on.")
-
-    if debug:
+        logging.info(f"Using annotator '{annotator}' to store annotations.")
         logging.info(f"Using directory '{usage_dir}' for usage data.")
-
-    if custom_dir is None:
-        custom_dir = usage_dir
-    if debug:
-        logging.info(f"Using directory '{custom_dir}' to store judgements.")
-
-    if debug:
-        logging.info(f"Using filename '{custom_filename}' to store judgements.")
-
-
-def specify_xl_lexeme_annotator(thresholds: list[int]) -> str:
-    if thresholds is not None and len(thresholds) == 1:
-        return "XL-Lexeme-Binary"
-    elif thresholds is not None and len(thresholds) == 3:
-        return "XL-Lexeme-Multi-Threshold"
-    else:
-        return "XL-Lexeme-Cosine"
+        logging.info(f"Using directory '{annotation_dir}' to store annotations.")
+        logging.info(f"Using filename '{annotation_filename}' to store annotations.")
 
 
 if __name__ == '__main__':
@@ -73,21 +81,24 @@ if __name__ == '__main__':
                       help="Should be XL-Lexeme or Random. Default XL-Lexeme.")
 
     parser.add_option("-p", "--prefix", dest="prefix", type=str, default="",
-                      help="Prefix for the usage, instance and judgement files. Default empty string.")
+                      help="Prefix for the usage, instance and annotation files. Default empty string.")
     parser.add_option("-u", "--usage_dir", dest="usage_dir", required=True,
                       help="Directory containing uses and instances data. Required.")
-    parser.add_option("-c", "--judgment_dir", dest="judgment_dir", type=str,
-                      help="Directory to store custom judgements. Defaults to the value given for usage_dir.")
-    parser.add_option("-f", "--judgment_filename", dest="judgment_filename", default="judgements.csv",
-                      help="Filename to store custom judgements. Default 'judgements.csv'.")
+    parser.add_option("-c", "--annotation_dir", dest="annotation_dir", type=str,
+                      help="Directory to store custom annotations. Defaults to the value given for usage_dir.")
+    parser.add_option("-f", "--annotation_filename", dest="annotation_filename", default="annotations.csv",
+                      help="Filename to store custom annotations. Default 'annotations.csv'.")
 
     parser.add_option("-d", "--debug", dest="debug", action="store_true", help="Enable debug mode.")
     parser.add_option("-t", "--thresholds", dest="thresholds", type=list[int], default=None,
                       help="Thresholds for cutoff if mapping to labels is requested.")
-    parser.add_option("-s", "--sub-word", dest="sub-word", help="provide sub-word subword_aggregation.")
+    parser.add_option("-s", "--settings_location", default='./settings/repository-settings.json',
+                      dest="settings_location", type=str, help="Default: './settings/repository-settings.json'")
     (options, args) = parser.parse_args()
     logging.info(options)
-    default_thresholds = [0.2, 0.4, 0.6]
 
-    main(options.annotator, options.usage_dir, options.custom_dir, options.custom_filename, options.prefix,
-         options.debug, options.thresholds)
+    if options.annotation_dir is None:
+        options.annotation_dir = options.usage_dir
+
+    main(options.annotator, options.usage_dir, options.annotation_dir, options.annotation_filename, options.prefix,
+         options.debug, options.thresholds, options.settings_location)
