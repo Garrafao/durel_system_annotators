bdee575ce3fd9c32f445917c751c7fc60ac4a523
AinaIanemahy <paulinesander@gmx.de>
2024-04-08 15:57:31 +0200

    Refactor annotation pipeline and update repository settings
    This commit updated the annotation pipeline in 'pipeline.py' to remove bugs and close logical gaps. Code for 'authenticate', 'get_tasks', 'get_instances' and 'annotate' functions are updated to better handle tasks and instances. It also changes routes and parameters in 'repository-settings.json' to debug interactions with the server. In addition, '.gitignore' is updated to ignore log files.

--- code/pipeline.py
+++ code/pipeline.py
@@ -1,8 +1,11 @@
+import concurrent.futures
 import json
 import logging
 import os
 import random
 import subprocess
+from datetime import datetime
+
 import torch
 
 import requests
@@ -18,12 +21,22 @@ def get_gpu_usage():
     return gpu_load
 
 
-def authenticate():
+def create_file(path):
+    directory = os.path.dirname(path)
+    os.makedirs(directory, exist_ok=True)
+    with open(path, 'a'):
+        pass
 
-    url = settings['url'] + settings['auth_route']
+
+def authenticate():
+    login_url = settings['url'] + settings['login_route']
+    auth_url = settings['url'] + settings['auth_route']
 
     with requests.Session() as s:
-        r = s.post(url, json={
+        s.post(login_url, data={
+            'username': settings['username'],
+            'password': settings['password']})
+        r = s.post(auth_url, json={
             'username': settings['username'],
             'password': settings['password']})
         print(r.content)
@@ -32,17 +45,16 @@ def authenticate():
         return r.json()['jwt']
     else:
         print('Error: ' + str(r.status_code))
+        exit()
 
 
-# HELPER FUNCTIONS ########
-def update_task_status(task_id, status):
+def update_task_status(status):
     """
     Update the status of a task with the given task ID.
 
     Example:
     update_task_status(123, 'completed')
 
-    :param task_id: (int): The ID of the task to update the status for.
     :param status: (str): The new status of the task.
     """
     url = settings['url'] + settings['update_status_route'].format(task_id, status)
@@ -68,23 +80,18 @@ def get_tasks():
         - If the status code of the response is not 200, the method will exit with status code 1.
         - If no task is available (task ID is 0), the method will exit with status code 0.
     """
-    url = settings['url'] + settings['/tasks/next']
-    print("url for next task: " + url)
+    url = settings['url'] + settings['next_task_route']
     r = requests.get(url,
                      headers={'Authorization': auth})
 
     # If status code is not 200, exit
     if r.status_code != 200:
         exit(1)
-
     new_task = r.json()
 
-    # If no task is available, exit
-    if new_task['id'] == 0:
-        exit(0)
-
+    logging.info(datetime.now())
     logging.info(new_task)
-    logging.info("Annotator is: %s", task['annotatorType'])
+    logging.info("Annotator is: %s", new_task['annotatorType'])
     return new_task
 
 
@@ -92,10 +99,7 @@ def get_instances():
     """
     Retrieves instances from the server based on the provided word or project.
     """
-    if word is None:
-        url = settings['url'] + settings['instance_with_project_route'].format(project)
-    else:
-        url = settings['url'] + settings['instance_with_word_route'].format(project, word)
+    url = settings['url'] + settings['instance_route'].format(task_id)
 
     print(url)
     r = requests.get(url, headers={
@@ -103,14 +107,39 @@ def get_instances():
     })
 
     if r.status_code != 200:
-        update_task_status(task['id'], StatusEnum.TASK_FAILED.value)
+        logging.info("Failed to load file.")
+        update_task_status(StatusEnum.TASK_FAILED.value)
         exit(1)
 
-    with open('tmp/{}instances_with_token_index.csv'.format(prefix), 'w') as f:
-        f.write(r.content.decode('utf-8'))
+    new_prefixes = []
+    content = r.content.decode('utf-8')
+    for batch in content[1:-2].split('\n, '):
+        prefix = str(random.randint(0, 1000))
+        file_path = './temp' + '/{}'.format(prefix) + settings['token_index_filename'] + settings['file_extension']
+        create_file(file_path)
+        with open(file_path, 'w') as f:
+            f.write(batch)
+        new_prefixes.append(prefix)
+    return new_prefixes
 
 
-def annotate():
+def perform_annotation(prefix):
+    """
+    Performs annotation for the given prefix.
+
+    :param prefix: The prefix to be used for annotation.
+    :type prefix: str
+    """
+    logging.info("Start annotation for prefix: " + prefix)
+    completed_process = annotate(prefix)
+    stderr_output = completed_process.stderr.decode('utf-8')
+    if stderr_output:
+        logging.error(stderr_output)
+    finish_annotation(prefix, completed_process)
+    delete_temporary_files(prefix)
+
+
+def annotate(prefix):
     """
     This method is used to run an annotation process based on the given annotator type. It determines the annotation
     script to use based on the annotator type and executes the script using subprocess.
@@ -118,36 +147,19 @@ def annotate():
 
     :return: The subprocess result object.
     """
-    def determine_script():
-        """
-        Determine the script based on the annotator type.
-
-        :return: str: The script path.
-        """
-        if annotator_type == "Random":
-            return settings['randomAnnotatorScript']
-        else:
-            return settings['xl-lexemeAnnotatorScript']
-
-    annotation_script_to_use = determine_script()
-
     with open('logs/subprocess.logs', 'w') as f:
-        if annotator_type == "XL-Lexeme-Binary":
+        if annotator_type == "XL-Lexeme":
             process = subprocess.run(
-                [python_env, annotation_script_to_use, '-u', 'tmp', '-p', prefix, '-o' 'label'], stdout=f,
-                stderr=subprocess.PIPE)
-        elif annotator_type == "XL-Lexeme-Cosine":
-            process = subprocess.run(
-                [python_env, annotation_script_to_use, '-u', 'tmp', '-p', prefix, '-o', 'distance'], stdout=f,
+                [python_env, 'code/annotate.py', '-a' 'XL-Lexeme', '-p', prefix, '-t', thresholds], stdout=f,
                 stderr=subprocess.PIPE)
         else:
             process = subprocess.run(
-                [python_env, annotation_script_to_use, '-u', 'tmp', '-p', prefix, "-d", '-a' 'Random'], stdout=f,
+                [python_env, 'code/annotate.py', '-p', prefix], stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE)
     return process
 
 
-def finish_annotation():
+def finish_annotation(prefix, completed_process):
     """
     Finish the annotation process.
 
@@ -155,38 +167,39 @@ def finish_annotation():
     it updates the task status to 'TASK_FAILED'. Otherwise, it uploads the judgements to the server.
     """
     if completed_process.returncode != 0:
-        update_task_status(task['id'], StatusEnum.TASK_FAILED.value)
+        update_task_status(StatusEnum.TASK_FAILED.value)
 
     # JUDGEMENT UPLOAD ########
     # Upload judgements
     else:
         url = settings['url'] + settings['upload_annotation_route']
-        print("url for upload votes: " + url)
         # build multipart form data for file upload with owner, project, phase and task id
-        files = [("files", open('tmp/{}annotations.csv'.format(prefix), 'rb'))]
+        files = [("files", open('temp/{}annotations.csv'.format(prefix), 'rb'))]
 
         r = requests.post(url,
                           headers={'Authorization': auth},
                           files=files,
                           data={'projectName': task["projectName"],
                                 'task_id': task['id']})
-        os.remove('tmp/{}judgements.csv'.format(prefix))
-        print(r.text)
+        os.remove('temp/{}annotations.csv'.format(prefix))
+        logging.info(r.text)
 
 
-def delete_temporary_files():
-    os.remove('tmp/{}instances.csv'.format(prefix))
+def delete_temporary_files(prefix):
+    os.remove('temp/{}instances.csv'.format(prefix))
 
 
 # 1 - Check GPU
-load = get_gpu_usage()
-if load > 70:
-    exit("GPU has too much load!")
+if torch.cuda.is_available():
+    load = get_gpu_usage()
+    if load > 70:
+        exit("GPU has too much load!")
 
 # 2 - Setup
 with open('settings/repository-settings.json') as settings_file:
     settings = json.load(settings_file)
-logging.basicConfig(filename=settings['logFilePath'], filemode='a', format=settings['logFormatting'],
+create_file(settings['logFilePath'])
+logging.basicConfig(filename=settings['logFilePath'], filemode='a', format=settings['log_formatting'],
                     level=logging.INFO)
 
 python_env = os.path.join(os.getcwd(), settings['envName'], "bin", "python")
@@ -195,18 +208,19 @@ auth = 'Bearer ' + authenticate()
 
 # 3 - Get task
 task = get_tasks()
+task_id = task["id"]
 project = task["projectName"]
 word = task["word"]
 annotator_type = task["annotatorType"]
-prefix = str(random.randint(0, 1000))
+thresholds = [float(threshold) for threshold in task["thresholdValues"].split(',')] if task["thresholdValues"] else None
+logging.info("Thresholds: {}".format(thresholds))
 
-# 4 - Annotation
-logging.info("Start annotation")
-completed_process = annotate()
-# Capture stderr output from the completed process
-stderr_output = completed_process.stderr.decode('utf-8')
-if stderr_output:
-    logging.error(stderr_output)
+# 4 - Get instances
+prefixes = get_instances()
 
-# 5 - Finish annotation
-finish_annotation()
+# 5 - Annotation
+# Using ProcessPoolExecutor to handle parallel tasks
+with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    executor.map(perform_annotation, prefixes)
+
+logging.info("Finished")
